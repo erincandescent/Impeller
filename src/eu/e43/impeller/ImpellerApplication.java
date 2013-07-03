3b0751e4fe6ffb6105a55feccc8e654133e65746
Owen Shepherd <owen.shepherd@e43.eu>
2013-07-04 00:59:32 +0100

    IMP-1 #resolve Automatically enable synchronization of the feed for pre-existing users. Mark version 0.4.4

--- src/eu/e43/impeller/ImpellerApplication.java
+++ src/eu/e43/impeller/ImpellerApplication.java
@@ -1,10 +1,18 @@
 package eu.e43.impeller;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.atlassian.jconnect.droid.Api;
 
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
+
+import eu.e43.impeller.account.Authenticator;
+import eu.e43.impeller.content.PumpContentProvider;
 
 @ReportsCrashes(
         formKey = "",
@@ -19,5 +27,27 @@ public class ImpellerApplication extends Application {
     public void onCreate() {
         Api.init(this);
         super.onCreate();
+
+        int versionCode;
+        try {
+            PackageInfo packageInfo = null;
+            packageInfo = getPackageManager()
+                    .getPackageInfo(getPackageName(), 0);
+            versionCode = packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // That'll be the day...
+            throw new RuntimeException(e);
+        }
+
+        SharedPreferences prefs = getSharedPreferences("Impeller", MODE_PRIVATE);
+        int oldVersion = prefs.getInt("version", 0);
+
+        if(oldVersion < 16) {
+            AccountManager mgr = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+            Account[] accts = mgr.getAccountsByType(Authenticator.ACCOUNT_TYPE);
+            for(Account a : accts) {
+                getContentResolver().setSyncAutomatically(a, PumpContentProvider.AUTHORITY, true);
+            }
+        }
     }
 }
