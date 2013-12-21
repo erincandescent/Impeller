5028ebae39c7b6da02f0652a0971a972293daac0
Owen Shepherd <owen.shepherd@e43.eu>
2013-12-21 19:46:12 +0000

    IMP-465 IMP-466 #resolve Implemented :-D

--- src/eu/e43/impeller/activity/PostActivity.java
+++ src/eu/e43/impeller/activity/PostActivity.java
@@ -1,7 +1,9 @@
 package eu.e43.impeller.activity;
 
-
-
+import org.htmlcleaner.CompactHtmlSerializer;
+import org.htmlcleaner.ContentNode;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,6 +40,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Iterator;
 
@@ -45,6 +49,8 @@ import eu.e43.impeller.Constants;
 import eu.e43.impeller.LocationServices;
 import eu.e43.impeller.PostTask;
 import eu.e43.impeller.content.PumpContentProvider;
+import eu.e43.impeller.ogp.MetaElement;
+import eu.e43.impeller.ogp.OpenGraph;
 import eu.e43.impeller.uikit.LocationAdapter;
 import eu.e43.impeller.uikit.PumpHtml;
 import eu.e43.impeller.R;
@@ -62,27 +68,39 @@ public class PostActivity extends ActivityWithAccount {
     private static final int TYPE_NOTE    = 0;
     private static final int TYPE_COMMENT = 1;
     private static final int TYPE_IMAGE   = 2;
+    private static final int TYPE_VIDEO   = 3;
 
     // UI widgets
-
     EditText        m_title;
+    ImageView       m_postType;
     ImageView       m_imageView;
 	EditText 	    m_content;
     Spinner         m_location;
     CheckBox        m_isPublic;
     ProgressDialog  m_progress;
-
-    Uri             m_extraUri;
-	Account  	    m_account;
-	JSONObject	    m_inReplyTo = null;
-    int             m_type;
     LocationAdapter m_locations;
-	
+
+    // Object properties
+    int             m_type;             // objectType
+    Uri             m_imageUri;         // Image URI
+	JSONObject	    m_inReplyTo = null; // inReplyTo
+    String          m_embedCode;        // embedCode
+    JSONObject      m_videoLink = null; // Video medialink
+    // Object properties which the UI may change:
+    String          m_proposedTitle;
+    String          m_proposedText;
+    String          m_proposedHTML;
+    String          m_sourceLink;
+
+
+    protected void onCreateEx(Bundle _) {
+        setContentView(R.layout.activity_post);
+    }
+
 	@Override
-	protected void onCreateEx(Bundle _) {
+	protected void gotAccount(Account a_, Bundle _) {
         Intent intent = getIntent();
         String type = intent.getType();
-        setContentView(R.layout.activity_post);
 
         Log.v(TAG, "MIME Type is " + type);
         if(type == null || type.startsWith("text/")) {
@@ -93,8 +111,8 @@ public class PostActivity extends ActivityWithAccount {
             }
         } else if(type.startsWith("image/")) {
             m_type = TYPE_IMAGE;
-            m_extraUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            if(m_extraUri == null) {
+            m_imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if(m_imageUri == null) {
                 Log.e(TAG, "No image URI?");
                 setResult(RESULT_CANCELED);
                 finish();
@@ -104,34 +122,89 @@ public class PostActivity extends ActivityWithAccount {
             finish();
         }
 
+        if(intent.hasExtra(Intent.EXTRA_SUBJECT)) {
+            m_proposedTitle = intent.getStringExtra(Intent.EXTRA_SUBJECT);
+        }
+
+        if(intent.hasExtra(EXTRA_HTML_TEXT)) {
+            m_proposedHTML = intent.getStringExtra(EXTRA_HTML_TEXT);
+        }
+
+        if(intent.hasExtra(Intent.EXTRA_TEXT)) {
+            m_proposedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        }
+
+        if(m_proposedText != null) {
+            Log.v(TAG, "Has proposed text... " + m_proposedText);
+            try {
+                URI uri = new URI(m_proposedText.trim());
+
+                Log.v(TAG, "Proposed text is an URL " + uri);
+                if("http".equals(uri.getScheme()) || "https".equals(uri.getScheme())) {
+                    Log.v(TAG, "Performing discovery on URL " + uri);
+                    new DiscoveryTask().execute(uri);
+                    return;
+                }
+
+            } catch(URISyntaxException ex) {
+                // Pass
+            }
+        }
+        // else
+        prepareUi();
+    }
+
+    private void prepareUi() {
+        // Prepare after doing full discovery
         m_title         = (EditText)  findViewById(R.id.title);
         m_imageView     = (ImageView) findViewById(R.id.image);
         m_location      = (Spinner)   findViewById(R.id.location);
         m_content       = (EditText)  findViewById(R.id.content);
         m_isPublic      = (CheckBox)  findViewById(R.id.isPublic);
+        m_postType      = (ImageView) findViewById(R.id.postType);
 
-        if(m_type == TYPE_IMAGE) {
+        switch(m_type) {
+            case TYPE_NOTE:
+            case TYPE_COMMENT:
+                m_postType.setImageResource(R.drawable.ic_note_dark);
+                break;
+
+            case TYPE_IMAGE:
+                m_postType.setImageResource(R.drawable.ic_picture_dark);
+                break;
+
+            case TYPE_VIDEO:
+                m_postType.setImageResource(R.drawable.ic_video_dark);
+                break;
+        }
+
+        if(m_imageUri != null) {
             m_imageView.setVisibility(View.VISIBLE);
-            SetupImageTask t = new SetupImageTask();
-            t.execute();
+            if(isImageLink()) {
+                getImageLoader().setImage(m_imageView, m_imageUri.toString());
+            } else {
+                SetupImageTask t = new SetupImageTask();
+                t.execute();
+            }
         }
 
-        if(intent.hasExtra(Intent.EXTRA_SUBJECT)) {
-            m_title.setText(intent.getStringExtra(Intent.EXTRA_SUBJECT));
+        if(m_proposedTitle != null) {
+            m_title.setText(m_proposedTitle);
         }
 
-        if(intent.hasExtra(EXTRA_HTML_TEXT)) {
-            PumpHtml.setFromHtml(this, m_content, intent.getStringExtra(EXTRA_HTML_TEXT));
-        } else if(intent.hasExtra(Intent.EXTRA_TEXT)) {
-            m_content.setText(intent.getCharSequenceExtra(Intent.EXTRA_TEXT));
+        if(m_proposedHTML != null) {
+            PumpHtml.setFromHtml(this, m_content, m_proposedHTML);
+        } else if(m_proposedText != null) {
+            m_content.setText(m_proposedText);
         }
 
-        if(ACTION_REPLY.equals(intent.getAction())) {
+        if(ACTION_REPLY.equals(getIntent().getAction())) {
             setTitle(R.string.title_activity_post_reply);
             m_title.setVisibility(View.GONE);
             m_title.setText("");
+
             try {
-                m_inReplyTo = new JSONObject(intent.getStringExtra("inReplyTo"));
+                m_inReplyTo = new JSONObject(getIntent().getStringExtra("inReplyTo"));
             } catch (JSONException e) {
                 Log.e(TAG, "Error parsing inReplyTo", e);
                 setResult(RESULT_CANCELED);
@@ -189,12 +262,165 @@ public class PostActivity extends ActivityWithAccount {
         super.onDestroy();
     }
 
+    private boolean isImageLink() {
+        return m_imageUri.getScheme().equals("http") || m_imageUri.getScheme().equals("https");
+    }
+
+    private class DiscoveryTask extends AsyncTask<URI, Void, Void> {
+        ProgressDialog m_discoveryProgress;
+
+        @Override
+        protected void onPreExecute() {
+            m_discoveryProgress = ProgressDialog.show(PostActivity.this,
+                    getString(R.string.just_a_moment),
+                    getString(R.string.getting_information), true);
+        }
+
+        @Override
+        protected Void doInBackground(URI... params) {
+            try {
+                OpenGraph og = new OpenGraph(params[0].toString(), true);
+
+                for(MetaElement elem : og.getProperties()) {
+                    Log.d(TAG, elem.getNamespace().getPrefix() + ":" + elem.getProperty() + "=" + elem.getContent());
+                }
+
+                String image = og.getContent("image");
+                if(m_imageUri == null && image != null) {
+                    if(og.getContent("image:secure_url") != null) {
+                        m_imageUri = Uri.parse(og.getContent("image:secure_url"));
+                        Log.i(TAG, "Has og:image:secure_url " + m_imageUri);
+                    } else {
+                        m_imageUri = Uri.parse(image);
+                        Log.i(TAG, "Has og:image " + m_imageUri);
+                    }
+                } else Log.i(TAG, "No og:image");
+
+                if(og.getContent("url") != null) {
+                    m_sourceLink = og.getContent("url");
+                    Log.i(TAG, "og:url " + m_sourceLink);
+                } else {
+                    m_sourceLink = params[0].toString();
+                }
+
+                if(og.getContent("title") != null)
+                    m_proposedTitle = og.getContent("title");
+
+                if(m_proposedText == null && og.getContent("description") != null)
+                    m_proposedText = og.getContent("description");
+
+                String ogType = og.getContent("type");
+
+                if(ogType != null && ogType.equals("video")) {
+                    String videoUrl = og.getContent("video");
+                    if(videoUrl != null) {
+                        if(og.getContent("video:secure_url") != null)
+                            videoUrl = og.getContent("video:secure_url");
+
+                        String videoType = og.getContent("video:type");
+
+                        if(videoUrl == null) {
+                            // pass
+                        } else {
+                            m_type = TYPE_VIDEO;
+
+                            HtmlCleaner cleaner = new HtmlCleaner();
+                            TagNode embedCode;
+
+                            Integer width  = null;
+                            Integer height = null;
+
+                            if(og.getContent("video:width") != null)
+                                width = Integer.parseInt(og.getContent("video:width"));
+
+                            if(og.getContent("video:height") != null)
+                                height = Integer.parseInt(og.getContent("video:height"));
+
+                            if(videoType != null && !videoType.startsWith("video/")) {
+                                embedCode = new TagNode("object");
+                                embedCode.addAttribute("data", videoUrl);
+                                embedCode.addAttribute("type", videoType);
+
+                                // For Flash on downlevel browsers
+                                TagNode movieParam = new TagNode("param");
+                                movieParam.addAttribute("movie", videoUrl);
+                                embedCode.addChild(movieParam);
+                            } else { // video/*
+                                embedCode = new TagNode("video");
+                                embedCode.addAttribute("src", videoUrl);
+                                if(videoType != null)
+                                    embedCode.addAttribute("type", videoType);
+                            }
+
+                            if(width != null)
+                                embedCode.addAttribute("width", width.toString());
+
+                            if(height != null)
+                                embedCode.addAttribute("height", height.toString());
+
+                            // Image and link fallback!
+                            TagNode linkNode = new TagNode("a");
+                            linkNode.addAttribute("href", m_sourceLink);
+                            if(isImageLink()) {
+                                TagNode imgNode = new TagNode("img");
+                                imgNode.addAttribute("src", m_imageUri.toString());
+                                imgNode.addAttribute("alt", "Watch video at source");
+                                linkNode.addChild(imgNode);
+                            } else {
+                                linkNode.addChild(new ContentNode("Watch video at source"));
+                            }
+
+                            if(og.getContent(OpenGraph.TWITTER_NS, "player") != null) {
+                                // iframe fallback
+                                TagNode iframeNode = new TagNode("iframe");
+                                iframeNode.addAttribute("src",    og.getContent(OpenGraph.TWITTER_NS, "player"));
+                                if(width != null)
+                                    iframeNode.addAttribute("width", width.toString());
+                                if(height != null)
+                                    iframeNode.addAttribute("height", height.toString());
+                                iframeNode.addChild(linkNode);
+                                embedCode.addChild(iframeNode);
+                            } else {
+                                embedCode.addChild(linkNode);
+                            }
+
+                            TagNode dummyNode = new TagNode("div");
+                            dummyNode.addChild(embedCode);
+
+                            m_embedCode = cleaner.getInnerHtml(dummyNode);
+                            Log.v(TAG, "Video embed code: " + m_embedCode);
+
+                            m_videoLink = new JSONObject();
+                            m_videoLink.put("url", videoUrl);
+                            if(videoType != null)
+                                m_videoLink.put("type",   videoType);
+                            if(width != null)
+                                m_videoLink.put("width", width);
+                            if(height != null)
+                                m_videoLink.put("height", height);
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.w(TAG, "Error doing discovery", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            m_discoveryProgress.dismiss();
+            prepareUi();
+        }
+    }
+
     private class SetupImageTask extends AsyncTask<Void, Void, Bitmap> {
 
         @Override
         protected Bitmap doInBackground(Void... voids) {
             try {
-                InputStream is = getContentResolver().openInputStream(m_extraUri);
+                InputStream is = getContentResolver().openInputStream(m_imageUri);
                 Bitmap bmp = BitmapFactory.decodeStream(is);
                 is.close();
                 Display disp = getWindowManager().getDefaultDisplay();
@@ -236,16 +462,21 @@ public class PostActivity extends ActivityWithAccount {
         if(m_title != null) m_title.clearComposingText();
         if(m_content != null) m_content.clearComposingText();
 
-        m_progress = ProgressDialog.show(this, "Posting...", "Submitting post");
+        m_progress = ProgressDialog.show(this,
+                getString(R.string.posting_title),
+                getString(R.string.status_submitting_post));
         m_progress.setIndeterminate(true);
-
 
         try {
             switch(m_type) {
                 case TYPE_IMAGE:
-                    PostImageTask t = new PostImageTask();
-                    t.execute(m_extraUri);
-                    return;
+                    if(!isImageLink()) {
+                        PostImageTask t = new PostImageTask();
+                        t.execute(m_imageUri);
+                        return;
+                    }
+
+                    obj.put("objectType", "image");
 
                 case TYPE_NOTE:
                     obj.put("objectType", "note");
@@ -254,10 +485,25 @@ public class PostActivity extends ActivityWithAccount {
                     obj.put("objectType", "comment");
                     break;
 
+                case TYPE_VIDEO:
+                    obj.put("objectType", "video");
+                    if(m_videoLink != null)
+                        obj.put("stream", m_videoLink);
+                    if(m_embedCode != null)
+                        obj.put("embedCode", m_embedCode);
+                    break;
+
                 default:
                     throw new RuntimeException("Bad type");
 
             }
+
+            if(m_imageUri != null) {
+                JSONObject image = new JSONObject();
+                image.put("url", m_imageUri);
+                obj.put("image", image);
+            }
+
             postPhase2(obj);
         } catch(Exception ex) {
             Log.e(TAG, "Error creating object", ex);
@@ -265,7 +511,24 @@ public class PostActivity extends ActivityWithAccount {
         }
     }
 
-    class PostImageTask extends AsyncTask<Object, Void, JSONObject> {
+    class PostImageTask extends AsyncTask<Object, Long, JSONObject> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            m_progress.setMessage(getString(R.string.status_uploading_image));
+        }
+
+        @Override
+        protected void onProgressUpdate(Long... values) {
+            if(values[0] != AssetFileDescriptor.UNKNOWN_LENGTH) {
+                m_progress.setIndeterminate(false);
+                m_progress.setMax(values[0].intValue());
+                m_progress.setProgress(values[1].intValue());
+            } else {
+                m_progress.setIndeterminate(true);
+            }
+        }
+
         @Override
         protected JSONObject doInBackground(Object... uris) {
             Log.v(TAG, "Posting image");
@@ -297,8 +560,11 @@ public class PostActivity extends ActivityWithAccount {
                 OutputStream os = conn.getOutputStream();
 
                 byte[] buf = new byte[4096];
+                long progress = 0;
                 int read = is.read(buf);
                 while(read > 0) {
+                    progress += read;
+                    publishProgress(length, progress);
                     os.write(buf, 0, read);
                     read = is.read(buf);
                 }
@@ -320,11 +586,13 @@ public class PostActivity extends ActivityWithAccount {
 
         @Override
         protected void onPostExecute(JSONObject obj) {
+            m_progress.setIndeterminate(true);
             postPhase2(obj);
         }
     }
 
 	private void postPhase2(JSONObject obj) {
+        m_progress.setMessage(getString(R.string.status_submitting_post));
         if(obj == null) {
             Toast.makeText(this, "Error creating object", Toast.LENGTH_SHORT).show();
             dismissProgress();
